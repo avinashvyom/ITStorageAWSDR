4c107c0396c858b3c5dbb10f97dccdd9def5b715
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2023-09-12 18:10:13 +0530

    REFACTORED: Removed unnecessary code

--- src/main/java/com/vyomlabs/S3ObjectFetch.java
+++ src/main/java/com/vyomlabs/S3ObjectFetch.java
@@ -3,10 +3,10 @@ package com.vyomlabs;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.nio.file.Path;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.log4j.Logger;
+
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.GetObjectRequest;
@@ -15,16 +15,15 @@ import com.amazonaws.services.s3.model.S3ObjectInputStream;
 
 public class S3ObjectFetch {
 
-	private String USAGE_REPORT_FILE_NAME = "Usage_Report_" + getCurrentMonthAndYear() + ".csv";
-	private String COST_REPORT_FILE_NAME = "Cost_Report_" + getCurrentMonthAndYear() + ".csv";
+	private final static Logger logger = Logger.getLogger(S3ObjectFetch.class);
 	
-
-	public File getCostReport(AmazonS3 s3Client, String BUCKET_NAME) {
-		//PropertyConfigurator.configure("src/main/resources/log4j.properties");
+	public File getReportFromBucket(AmazonS3 s3Client, String BUCKET_NAME, String REPORT_NAME) {
 		try {
-			String path = Path.of("").toAbsolutePath().toString() + "\\" + COST_REPORT_FILE_NAME;
+			String path = Path.of("").toAbsolutePath().toString() + "\\" + REPORT_NAME;
 
-			GetObjectRequest getObjectRequest = new GetObjectRequest(BUCKET_NAME, COST_REPORT_FILE_NAME);
+			logger.info("report file Name : "+REPORT_NAME);
+
+			GetObjectRequest getObjectRequest = new GetObjectRequest(BUCKET_NAME, REPORT_NAME);
 
 			S3Object s3Object = s3Client.getObject(getObjectRequest);
 			S3ObjectInputStream inputStream = s3Object.getObjectContent();
@@ -35,37 +34,11 @@ public class S3ObjectFetch {
 			inputStream.close();
 			outputStream.close();
 
-			return new File(Path.of("").toAbsolutePath().toString() + "\\" + COST_REPORT_FILE_NAME);
+			return new File(Path.of("").toAbsolutePath().toString() + "\\" + REPORT_NAME);
 		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
 		}
-	}
-
-	public File getUsageReport(AmazonS3 s3Client, String BUCKET_NAME) {
-		//PropertyConfigurator.configure("src/main/resources/log4j.properties");
-		try {
-			String path = Path.of("").toAbsolutePath().toString() + "\\" + USAGE_REPORT_FILE_NAME;
-
-			GetObjectRequest getObjectRequest = new GetObjectRequest(BUCKET_NAME, USAGE_REPORT_FILE_NAME);
-			S3Object s3Object = s3Client.getObject(getObjectRequest);
-			S3ObjectInputStream inputStream = s3Object.getObjectContent();
-			FileOutputStream outputStream = new FileOutputStream(path);
-
-			IOUtils.copy(inputStream, outputStream);
-			inputStream.close();
-			outputStream.close();
-
-			return new File(Path.of("").toAbsolutePath().toString() + "\\" + USAGE_REPORT_FILE_NAME);
-		} catch (Exception e) {
-			// TODO: handle exception
-			e.printStackTrace();
-			return null;
-		}
-	}
-
-	private String getCurrentMonthAndYear() {
-		return new SimpleDateFormat("MMM-YYYY").format(new Date());
 	}
 
 }
