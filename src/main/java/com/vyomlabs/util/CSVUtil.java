6fd59e8a860f79eef63a611cc780aade68c473ba
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2023-10-04 18:23:48 +0530

    REFACTORED: code to get failure or success records

--- src/main/java/com/vyomlabs/util/CSVUtil.java
+++ src/main/java/com/vyomlabs/util/CSVUtil.java
@@ -1,17 +1,19 @@
 package com.vyomlabs.util;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 import org.apache.log4j.Logger;
 
+import com.vyomlabs.entity.FileBackupDetails;
 import com.vyomlabs.entity.FileUploadStatus;
 
 public class CSVUtil {
@@ -41,20 +43,33 @@ public class CSVUtil {
 	
 	private int getCountOnCondition(FileUploadStatus fileUploadStatus) throws FileNotFoundException, IOException {
 		
-		List<List<String>> records = new ArrayList<List<String>>();
+		List<FileBackupDetails> records = new ArrayList<>();
 		// File file = new File(Path.of("").toAbsolutePath().toString() + "/" +
 		// getCSVFileName());
 		logger.info("File Path is (from CSVUtil class) : " + csvFile.getAbsolutePath());
-		try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
-			String line;
-			while ((line = br.readLine()) != null) {
-				String[] values = line.split(",");
-				records.add(Arrays.asList(values));
-			}
-		}
 		
-		int filteredRecords = records.stream().filter(record -> record.get(4).equals(fileUploadStatus.name())).collect(Collectors.toList()).size();
-		logger.info("Filtered Records are : " + filteredRecords);
+		FileReader fileReader = new FileReader(csvFile);
+		CSVParser csvParser = new CSVParser(fileReader, CSVFormat.DEFAULT);
+		for (CSVRecord csvRecord : csvParser) {
+			FileBackupDetails fileBackupDetails = FileBackupDetails.builder().fileName(csvRecord.get(0))
+					.filePathOnLocalDrive(csvRecord.get(1)).filePathInS3(csvRecord.get(2)).uploadDate(csvRecord.get(3))
+					.uploadStatus(csvRecord.get(4)).fileStatus(csvRecord.get(5)).fileSize(csvRecord.get(6)).build();
+			records.add(fileBackupDetails);
+			//csvRecord.stream().filter(record -> record.get(4).equals(fileUploadStatus.name()));
+		}
+
+		csvParser.close();
+		fileReader.close();
+//		try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+//			String line;
+//			while ((line = br.readLine()) != null) {
+//				String[] values = line.split(",");
+//				records.add(Arrays.asList(values));
+//			}
+//		}
+		
+		int filteredRecords = records.stream().filter(record -> record.getUploadStatus().equals(fileUploadStatus.name())).collect(Collectors.toList()).size();
+		logger.info("no of filtered Records are : " + filteredRecords);
 		return filteredRecords;
 	}
 	
